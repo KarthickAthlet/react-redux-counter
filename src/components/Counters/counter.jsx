5874a169ab058a4834f9ab4481ee6e5792faf7c7
DESKTOP-FQBJS4T\Hacky_Shaggy <karthick.psgtec12@gmail.com>
2019-02-28 02:44:42 +0530

    Added Proptypes

--- src/components/Counters/counter.jsx
+++ src/components/Counters/counter.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as ActionCreator from "./../../Actions";
+import PropTypes from "prop-types";
 
 class Counter extends Component {
   render() {
@@ -25,6 +26,10 @@ class Counter extends Component {
     );
   }
 }
+
+Counter.propTypes = {
+  counter: PropTypes.number
+};
 
 const mapStateToProps = state => {
   return { counter: state };
